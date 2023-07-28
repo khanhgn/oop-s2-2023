0b65c0a4881184633a86b973477d476814982870
Khanh Nguyen <khanhnguyen@student-10-201-19-186.wireless.adelaide.edu.au>
2023-07-28 16:18:45 +0930

    yes

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -5,7 +5,7 @@ double sum_even(double array[], int n) {
   if (n < 1) {
     return 0;
   }
-  int sum = array[1];
+  int sum = array[0];
   for (int i = 1; i < n; i++) {
     if (i % 2 == 0) {
       sum += array[i];
