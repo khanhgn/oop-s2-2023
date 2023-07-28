92e24fcc495128481c6a4498a973cc35eba376cc
Khanh Nguyen <khanhnguyen@student-10-201-19-186.wireless.adelaide.edu.au>
2023-07-28 17:03:27 +0930

    yes

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -8,18 +8,10 @@ bool is_fanarray(int array[], int n) {
   if (n < 1) {
     return false;
   }
-  for (int i = 1; i < n / 2; i++) {
-    if (array[i - 1] < array[i]) {
-      asCondition = true;
+  for (int i = 0; i < n / 2; i++) {
+    if (array[i] > array[i + 1] || array[i] != array[n - i - 1]) {
+      return false;
     }
   }
-  for (int i = n / 2 + 1; i < n; i++) {
-    if (array[i - 1] > array[i]) {
-      desCondition = true;
-    }
-  }
-  if (desCondition == 1 && asCondition == 1) {
-    condition = true;
-  }
-  return condition;
+  return true;
 }
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 extern bool is_fanarray(int array[], int n);
 int main() {
-  int n = 4;
-  int array[4] = {1, 2, 2, 1};
+  int n = 5;
+  int array[] = {1, 2, 1, 2, 1};
   std::cout << is_fanarray(array, n) << std::endl;
   return 0;
 }
