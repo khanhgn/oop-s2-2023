ebc651cf1a17c1257e855dcfd52ad597dcb35079
Khanh Nguyen <khanhnguyen@student-10-201-19-208.wireless.adelaide.edu.au>
2023-08-02 11:46:11 +0930

    eyss

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int is_identity(int array[10][10]) {
   bool isTrue = false;
-  bool isDiagonal = false;
-  bool isZero = false;
+  bool isDiagonal = true;
+  bool isZero = true;
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
       if (i == j) {
-        if (array[i][j] == 1) {
-          isDiagonal = true;
+        if (array[i][j] != 1) {
+          isDiagonal = false;
         }
       } else {
-        if (array[i][j] == 0) {
-          isZero = true;
+        if (array[i][j] != 0) {
+          isZero = false;
         }
       }
     }
