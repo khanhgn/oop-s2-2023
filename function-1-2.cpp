07df019575a7b23b97c048f3cf7c85f4578f6eb8
Khanh Nguyen <khanhnguyen@student-10-201-19-208.wireless.adelaide.edu.au>
2023-08-02 11:38:55 +0930

    yes

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -5,8 +5,8 @@ int is_identity(int array[10][10]) {
   bool isTrue = false;
   bool isDiagonal = false;
   bool isZero = false;
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
       if (i == j) {
         if (array[i][j] == 1) {
           isDiagonal = true;
