eb24a3f6916387f19722e1245ae268ced1800224
Khanh Nguyen <khanhnguyen@student-10-201-19-058.wireless.adelaide.edu.au>
2023-08-09 16:08:59 +0930

    yes

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -19,17 +19,17 @@ bool is_array_palindrome(int integers[], int length) {
   }
   for (int i = 0; i < length / 2; i++) {
     if (integers[i] != integers[length - i - 1]) {
-      return 1;
+      return false;
     }
   }
-  return 0;
+  return true;
 }
 
 int sum_integers(int integers[], int length) {
   if (length < 1) {
     return -1;
   }
-  if (is_array_palindrome(integers, length) == 1) {
+  if (is_array_palindrome(integers, length) == false) {
     return -2;
   }
   int sum = palindrome_sum(integers, length);
--- main-2-3.cpp
+++ main-2-3.cpp
@@ -4,8 +4,8 @@ using namespace std;
 extern int sum_integers(int integers[], int length);
 
 int main() {
-  int array[6] = {1, 2, 0, 0, 2, 1};
-  int length = 6;
+  int array[3] = {0, 1, 0};
+  int length = 3;
   cout << sum_integers(array, length) << endl;
   return 0;
 }
