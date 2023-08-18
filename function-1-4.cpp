c1bb04eb3997df3c6dac0d44b0a0eb28eb78f3d0
khanhgn <a1898379@adelaide.edu.au>
2023-08-18 11:23:55 +0930

    es

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -7,9 +7,6 @@ using namespace std;
 PersonList shallowCopyPersonList(PersonList pl) {
   PersonList shallow;
   shallow.numPeople = pl.numPeople;
-  for (int i = 0; i < shallow.numPeople; i++) {
-    shallow.people[i].name = pl.people[i].name;
-    shallow.people[i].age = pl.people[i].age;
-  }
+  shallow.people = pl.people;
   return shallow;
 }
