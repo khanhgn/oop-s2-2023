579d54b594929dddbdaa197a21806e5c1ee07bbb
khanhgn <127292124+khanhgn@users.noreply.github.com>
2023-10-24 20:58:59 +1030

    yes

--- Utils.h
+++ Utils.h
@@ -13,7 +13,11 @@ class Utils {
   double calculateDistance(std::tuple<int, int> pos1,
                            std::tuple<int, int> pos2) {
     int x1, y1, x2, y2;
-    x1 = get<1> int position = make_tuple(x, y);
-    return position;
+    x1 = get<0>(pos1);
+    y1 = get<1>(pos1);
+    x2 = get<0>(pos2);
+    y2 = get<1>(pos2);
+    double distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    return distance;
   }
 }
