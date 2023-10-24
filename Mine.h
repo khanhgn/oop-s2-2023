063a3980fe488910e2e37a34dd0fadc7909b8996
khanhgn <127292124+khanhgn@users.noreply.github.com>
2023-10-24 15:40:21 +1030

    check1

--- Mine.h
+++ Mine.h
@@ -1,15 +1,20 @@
-#include <tuple>
+#ifndef MINE_H
+#define MINE_H
 
-#include "Effect.h"
 #include "Explosion.h"
 #include "GameEntity.h"
-using namespace std;
 
 class Mine : public GameEntity {
  public:
-  Mine(int x, int y) : GameEntity(x, y, 'M') {}
+  Mine() : GameEntity(){};
+  Mine(int x, int y) : GameEntity(x, y, 'M'){};
   Explosion explode() {
-    type = 'X';
-    return Explosion(std::get<0>(position), std::get<1>(position));
+    this->setType('X');
+    std::tuple<int, int> position_ = this->getPos();
+    Explosion explosion(std::get<0>(position_), std::get<1>(position_));
+    return explosion;
   }
+  ~Mine(){};
 };
+
+#endif
