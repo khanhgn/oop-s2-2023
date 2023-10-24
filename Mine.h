9453592a23b63f20250433169dd192fb1e4bc4dc
khanhgn <127292124+khanhgn@users.noreply.github.com>
2023-10-24 21:23:29 +1030

    yes'

--- Mine.h
+++ Mine.h
@@ -5,5 +5,10 @@
 class Mine : public GameEntity {
  public:
   Mine(int x, int y) : GameEntity(x, y, 'M') {}
-  Explosion explode() { type = 'X'; }
+  Explosion explode() {
+    int x = get<0>(position);
+    int y = get<1>(position);
+    type = 'X';
+    Explosion e1(x, y);
+  }
 };
