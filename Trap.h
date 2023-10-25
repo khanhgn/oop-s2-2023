e1357cb927221573a01691a38657dbfdfe3c019d
khanhgn <127292124+khanhgn@users.noreply.github.com>
2023-10-25 15:25:52 +1030

    yes

---
+++ Trap.h
@@ -0,0 +1,16 @@
+#pragma once
+using namespace std;
+#include "Cell.h"
+#include "Effect.h"
+
+class Trap : public Cell, public Effect {
+ protected:
+  bool active;
+
+ public:
+  Trap(int x, int y) : Cell(x, y, 'T') {}
+  bool isActive() void apply(Cell& cell) {
+    cell.setType('T');
+    active = false;
+  }
+}
